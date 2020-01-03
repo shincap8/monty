e988507c1bed4120ee200e1fb600036892a73d6e
paisap <santiagoaldana58@gmail.com>
2020-01-03 17:25:58 -0500

    more functions

---
+++ functions.c
@@ -0,0 +1,131 @@
+#include "library.h"
+/**
+ * is_push- function that pushes an element to the stack
+ * @head: pointer to top of stack
+ * @number: number to insert to the stack
+ */
+void is_push(sstack_t **head, unsigned int number)
+{
+	sstack_t *new;
+
+	new = malloc(sizeof(sstack_t));
+	if (new == NULL)
+		exit(EXIT_FAILURE);
+	new->n = number;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+}
+/**
+ * is_pall- function that prints the stack
+ * @head: pointer to the top
+ * @number: integer
+ */
+void is_pall(sstack_t **head, unsigned int number)
+{
+	sstack_t *tmp = *head;
+
+	number = number;
+	if (head == NULL || *head == NULL)
+		exit(EXIT_FAILURE);
+	if (numbers[1] == 0)
+	{
+		while (tmp)
+		{
+			printf("%d\n", tmp->n);
+			tmp = tmp->next;
+		}
+	}
+	else
+	{
+		while (tmp->next)
+			tmp = tmp->next;
+		while (tmp)
+		{
+			printf("%d\n", tmp->n);
+			tmp = tmp->prev;
+		}
+	}
+}
+/**
+ * is_pint- function that prints the top of the stack
+ * @head: pointer of the top of the stack
+ * @number: integer
+ */
+void is_pint(sstack_t **head, unsigned int number)
+{
+	number = number;
+	if (head == NULL || *head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", numbers[0]);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
+/**
+ * is_pop- function that removes the top of the stack
+ * @head: top of the stack
+ * @number: integer
+ */
+void is_pop(sstack_t **head, unsigned int number)
+{
+	sstack_t *aux;
+
+	number = number;
+	if (*head == NULL || head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", numbers[0]);
+		exit(EXIT_FAILURE);
+	}
+	if (numbers[1] == 0)
+	{
+		if ((*head)->next != NULL)
+		{
+			aux = (*head)->next;
+			free(*head);
+			aux->prev = NULL;
+			*head = aux;
+		}
+		else
+			free(*head), *head = NULL;
+	}
+	else
+	{
+		while ((*head)->next)
+			*head = (*head)->next;
+
+		if ((*head)->prev != NULL)
+		{
+			aux = (*head)->prev;
+			free(*head);
+			aux->next = NULL;
+			*head = aux;
+		}
+		else
+			free(*head), *head = NULL;
+	}
+}
+/**
+ * is_swap- function that swaps the top two elements
+ * @head: pointer to top
+ * @number: integer
+ */
+void is_swap(sstack_t **head, unsigned int number)
+{
+	int x = 0;
+
+	number = number;
+	if ((*head)->next != NULL)
+	{
+		x = (*head)->next->n;
+		(*head)->next->n = (*head)->n;
+		(*head)->n = x;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", numbers[0]);
+		exit(EXIT_FAILURE);
+	}
+}
---
+++ functions2.c
@@ -0,0 +1,141 @@
+#include "library.h"
+/**
+ * is_add- adds the top two elements of the stack
+ * @head: pointer to top
+ * @number: integer
+ */
+void is_add(sstack_t **head, unsigned int number)
+{
+	sstack_t *aux;
+	int x = 0;
+
+	number = number;
+	if (numbers[1] == 0)
+	{
+		if ((*head)->next != NULL)
+		{
+			x = (*head)->next->n + (*head)->n;
+			aux = (*head)->next;
+			aux->n = x;
+			free(*head);
+			aux->prev = NULL;
+			*head = aux;
+		}
+		else
+		{
+			fprintf(stderr, "L%d: can't add, stack too short\n", numbers[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		while ((*head)->next)
+			*head = (*head)->next;
+		if ((*head)->prev != NULL)
+		{
+			x = (*head)->prev->n + (*head)->n;
+			aux = (*head)->prev;
+			free(*head);
+			aux->next = NULL;
+			*head = aux;
+		}
+		else
+		{
+			fprintf(stderr, "L%d: can't add, stack too short\n", numbers[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+/**
+ * is_nop- function that doesn't do anything
+ * @head: pointer to top
+ * @number: integer
+ */
+void is_nop(sstack_t **head, unsigned int number)
+{
+	head = head;
+	number = number;
+}
+/**
+ * is_sub - adds the top two elements of the stack
+ * @head: pointer to top
+ * @number: integer
+ */
+void is_sub(sstack_t **head, unsigned int number)
+{
+	sstack_t *aux;
+	int x = 0;
+
+	number = number;
+	if ((*head)->next != NULL)
+	{
+		x = (*head)->next->n - (*head)->n;
+		aux = (*head)->next;
+		aux->n = x;
+		free(*head);
+		aux->prev = NULL;
+		*head = aux;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", numbers[0]);
+		exit(EXIT_FAILURE);
+	}
+}
+/**
+ * is_div - adds the top two elements of the stack
+ * @head: pointer to top
+ * @number: integer
+ */
+void is_div(sstack_t **head, unsigned int number)
+{
+	sstack_t *aux;
+	int x = 0;
+
+	number = number;
+	if ((*head)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", numbers[0]);
+		exit(EXIT_FAILURE);
+	}
+	if ((*head)->next != NULL)
+	{
+		x = (*head)->next->n / (*head)->n;
+		aux = (*head)->next;
+		aux->n = x;
+		free(*head);
+		aux->prev = NULL;
+		*head = aux;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", numbers[0]);
+		exit(EXIT_FAILURE);
+	}
+}
+/**
+ * is_mul - adds the top two elements of the stack
+ * @head: pointer to top
+ * @number: integer
+ */
+void is_mul(sstack_t **head, unsigned int number)
+{
+	sstack_t *aux;
+	int x = 0;
+
+	number = number;
+	if ((*head)->next != NULL)
+	{
+		x = (*head)->next->n * (*head)->n;
+		aux = (*head)->next;
+		aux->n = x;
+		free(*head);
+		aux->prev = NULL;
+		*head = aux;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", numbers[0]);
+		exit(EXIT_FAILURE);
+	}
+}
